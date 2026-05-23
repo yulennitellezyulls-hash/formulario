ca9c1b69bfdff8608e07fde08b356f89985c558f
yulennitellezyulls-hash <yulennitellezyulls@gmail.com>
2026-05-23 13:25:43 -0600

    Implement form validation on submit
    Added form validation to ensure all fields are filled before submission.

--- script.js
+++ script.js
@@ -1,83 +1,14 @@
-import { initializeApp }
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+document.getElementById("formulario").addEventListener("submit", function(e){
 
-import {
-getFirestore,
-collection,
-addDoc
-}
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+    let nombre = document.getElementById("Nombre").value.trim();
+    let apellidoP = document.getElementById("ApellidoP").value.trim();
+    let apellidoM = document.getElementById("ApellidoM").value.trim();
+    let fecha = document.getElementById("fechanacimiento").value.trim();
+    let curp = document.getElementById("Curp").value.trim();
 
-
-const firebaseConfig = {
-
-apiKey: "PEGA_AQUI",
-
-authDomain: "PEGA_AQUI",
-
-projectId: "PEGA_AQUI",
-
-storageBucket: "PEGA_AQUI",
-
-messagingSenderId: "PEGA_AQUI",
-
-appId: "PEGA_AQUI"
-
-};
-
-
-const app = initializeApp(firebaseConfig);
-
-const db = getFirestore(app);
-
-
-const formulario =
-document.getElementById("formulario");
-
-
-formulario.addEventListener("submit",
-async(e)=>{
-
-e.preventDefault();
-
-const nombre =
-document.getElementById("nombre").value;
-
-const correo =
-document.getElementById("correo").value;
-
-const mensaje =
-document.getElementById("mensaje").value;
-
-
-try{
-
-await addDoc(
-collection(db,"contactos"),
-{
-
-nombre:nombre,
-
-correo:correo,
-
-mensaje:mensaje,
-
-fecha:new Date()
-
-}
-
-);
-
-alert("Mensaje enviado 💙");
-
-formulario.reset();
-
-}catch(error){
-
-alert("Error");
-
-console.log(error);
-
-}
+    if(nombre === "" || apellidoP === "" || apellidoM === "" || fecha === "" || curp === ""){
+        e.preventDefault();
+        alert("Todos los campos son obligatorios");
+    }
 
 });
